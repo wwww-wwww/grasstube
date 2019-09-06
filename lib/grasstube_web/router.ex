aa03b499443991f2d5c169c0dd8feb141ed437ed
w <wvvwvvvvwvvw@gmail.com>
2019-09-05 20:08:30 -0700

    Update router.ex

--- lib/grasstube_web/router.ex
+++ lib/grasstube_web/router.ex
@@ -17,15 +17,15 @@ defmodule GrasstubeWeb.Router do
   end
 
   pipeline :auth do
-    plug Grasstube.Pipeline
+    plug(Grasstube.Pipeline)
   end
 
   pipeline :logged_in do
-    plug Guardian.Plug.EnsureAuthenticated
+    plug(Guardian.Plug.EnsureAuthenticated)
   end
 
   pipeline :logged_out do
-    plug Guardian.Plug.EnsureNotAuthenticated
+    plug(Guardian.Plug.EnsureNotAuthenticated)
   end
 
   scope "/", GrasstubeWeb do
@@ -37,29 +37,30 @@ defmodule GrasstubeWeb.Router do
     get("/r/:room", PageController, :room)
 
     scope "/" do
-      pipe_through :logged_out
-      get "/sign_in", UserController, :sign_in_page
-      post "/sign_in", UserController, :sign_in
+      pipe_through(:logged_out)
+      get("/sign_in", UserController, :sign_in_page)
+      post("/sign_in", UserController, :sign_in)
 
-      get "/sign_up", UserController, :sign_up_page
-      post "/sign_up", UserController, :sign_up
+      get("/sign_up", UserController, :sign_up_page)
+      post("/sign_up", UserController, :sign_up)
     end
 
-    get "/sign_out", UserController, :sign_out
+    get("/sign_out", UserController, :sign_out)
     
-    get "/user/:username", UserController, :show_user
+    get("/user/:username", UserController, :show_user)
 
     scope "/" do
-      pipe_through :logged_in
-      post "/add_emote", UserController, :add_emote
-      post "/delete_emote", UserController, :delete_emote
+      pipe_through(:logged_in)
+      post("/add_emote", UserController, :add_emote)
+      post("/delete_emote", UserController, :delete_emote)
     end
-
-    scope "/api" do
-      pipe_through(:api)
-      get("/list_rooms", PageController, :list_rooms)
-      get("/emotes/r/:room", PageController, :emotes)
-    end
-
   end
+
+  scope "/api", GrasstubeWeb do
+    pipe_through(:api)
+    post("/auth", UserController, :auth)
+    get("/list_rooms", PageController, :list_rooms)
+    get("/emotes/r/:room", PageController, :emotes)
+  end
+
 end
