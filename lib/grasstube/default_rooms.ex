6a5f7bfaa3d1978b984f289be37fcf9a67062a15
w <wvvwvvvvwvvw@gmail.com>
2019-09-05 20:12:31 -0700

    Update default_rooms.ex

--- lib/grasstube/default_rooms.ex
+++ lib/grasstube/default_rooms.ex
@@ -1,7 +1,6 @@
 defmodule Grasstube.DefaultRooms do
   use Task
 
-  @default_rooms ["main"]
   @default_videos ["https://www.youtube.com/watch?v=KrapzeD00w8"]
 
   def start_link(_) do
@@ -9,11 +8,19 @@ defmodule Grasstube.DefaultRooms do
   end
 
   def run() do
-    Enum.each(@default_rooms, &(Grasstube.ProcessRegistry.create_room(&1, "wow")))
-    playlist = Grasstube.ProcessRegistry.lookup("main", :playlist)
-    Enum.each(@default_videos, &(GrasstubeWeb.PlaylistAgent.q_add(playlist, &1, "", "")))
+    Grasstube.ProcessRegistry.create_room("main", "w")
     chat = Grasstube.ProcessRegistry.lookup("main", :chat)
-    GrasstubeWeb.ChatAgent.add_mod(chat, "wow2")
-    GrasstubeWeb.ChatAgent.add_emotelist(chat, "ww")
+
+    Grasstube.ProcessRegistry.create_room("not main", "w")
+
+    playlist2 = Grasstube.ProcessRegistry.lookup("not main", :playlist)
+    Enum.each(@default_videos, &(GrasstubeWeb.PlaylistAgent.q_add(playlist2, &1, "", "")))
+    
+    chat2 = Grasstube.ProcessRegistry.lookup("not main", :chat)
+    #GrasstubeWeb.ChatAgent.add_mod(chat2, "clara")
+    #GrasstubeWeb.ChatAgent.add_mod(chat2, "mathguy2357")
+    #GrasstubeWeb.ChatAgent.add_emotelist(chat2, "w")
+    #GrasstubeWeb.ChatAgent.add_emotelist(chat2, "clara")
+    #GrasstubeWeb.ChatAgent.add_emotelist(chat2, "mathguy2357")
   end
 end
