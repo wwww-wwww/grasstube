2c895aa38f84ba5d46e1b9c21d0710edbba3ed63
w <wvvwvvvvwvvw@gmail.com>
2020-07-27 06:03:48 -0700

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -16,7 +16,9 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-config :phoenix, :json_library, Jason
+config :phoenix, 
+  json_library: Jason,
+  template_engines: [leex: Phoenix.LiveView.Engine]
 
 import_config "#{Mix.env()}.exs"
 
