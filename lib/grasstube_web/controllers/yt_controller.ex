09305a16feb077c0b8395258ebb063c1b956f3ea
w <wvvwvvvvwvvw@gmail.com>
2022-04-29 18:32:59 -0700

    check if there are youtube api keys

--- lib/grasstube_web/controllers/yt_controller.ex
+++ lib/grasstube_web/controllers/yt_controller.ex
@@ -2,40 +2,51 @@ defmodule GrasstubeWeb.YTController do
   use GrasstubeWeb, :controller
 
   def search(query) do
-    keys = Application.get_env(:grasstube, :youtube_api_keys)
+    Application.get_env(:grasstube, :youtube_api_keys)
+    |> case do
+      [] ->
+        %{success: 0, response: "No API keys configured"}
 
-    key = keys |> Enum.at(rem(Grasstube.YTCounter.inc(), length(keys)))
+      keys ->
+        key = Enum.at(keys, rem(Grasstube.YTCounter.inc(), length(keys)))
 
-    case HTTPoison.get("https://www.googleapis.com/youtube/v3/search", [],
-           params: %{key: key, part: "snippet", type: "video", maxResults: 50, q: query}
-         ) do
-      {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
-        case Jason.decode(body) do
-          {:ok, %{"items" => items}} ->
-            items =
-              items
-              |> Enum.map(fn %{
-                               "id" => %{
-                                 "videoId" => id
-                               },
-                               "snippet" => %{
-                                 "channelId" => channel_id,
-                                 "channelTitle" => channel_title,
-                                 "title" => title
-                               }
-                             } ->
-                %{id: id, title: title, channel_title: channel_title, channel_id: channel_id}
-              end)
-              |> Enum.reverse()
+        HTTPoison.get("https://www.googleapis.com/youtube/v3/search", [],
+          params: %{key: key, part: "snippet", type: "video", maxResults: 50, q: query}
+        )
+        |> case do
+          {:ok, %HTTPoison.Response{body: body, status_code: 200}} ->
+            case Jason.decode(body) do
+              {:ok, %{"items" => items}} ->
+                items =
+                  items
+                  |> Enum.map(fn %{
+                                   "id" => %{
+                                     "videoId" => id
+                                   },
+                                   "snippet" => %{
+                                     "channelId" => channel_id,
+                                     "channelTitle" => channel_title,
+                                     "title" => title
+                                   }
+                                 } ->
+                    %{
+                      id: id,
+                      title: title,
+                      channel_title: channel_title,
+                      channel_id: channel_id
+                    }
+                  end)
+                  |> Enum.reverse()
 
-            %{success: 1, items: items}
+                %{success: 1, items: items}
+
+              r ->
+                %{success: 0, response: inspect(r)}
+            end
 
           r ->
             %{success: 0, response: inspect(r)}
         end
-
-      r ->
-        %{success: 0, response: inspect(r)}
     end
   end
 
